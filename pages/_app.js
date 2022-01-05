5d3e8aa14a6f424e8512768e55e7c3494f494a1e
Musa Khan <57375561+MusaMoses@users.noreply.github.com>
2022-01-04 22:36:05 -0500

    Error handling for easier debugging

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import Header from '../components/Header'
-
+import Error from 'next/error'
 
 
 function MyApp({ Component, pageProps }) {
+  if (pageProps.error) {
+    return <Error statusCode={pageProps.error.statusCode} title={pageProps.error.message} />;
+  }
+
   return (
     <div className="app">
       <Head>
